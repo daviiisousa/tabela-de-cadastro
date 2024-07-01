a4bab2beed63d4ffdb324b71684d32b6346b5237
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-07-01 13:44:58 -0300

    modificando login

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function entrar(){
+    let email = document.getElementById('email');
+    let seenha = document.getElementById('senha');
+    if(email === ''){
+        alert('erro')
+    }
+    if(seenha === ''){
+        alert('erro')
+    }
+}
